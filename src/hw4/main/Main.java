e5fd3d57660d06f251aa810b9f09a7327aebc461
tgraber4 <tgraber4@gmail.com>
2025-04-21 10:35:50 -0500

    added classes

---
+++ src/hw4/main/Main.java
@@ -0,0 +1,10 @@
+package hw4.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/hw4/maze/classes/Grid.java
@@ -0,0 +1,5 @@
+package hw4.maze.classes;
+
+public class Grid {
+
+}
